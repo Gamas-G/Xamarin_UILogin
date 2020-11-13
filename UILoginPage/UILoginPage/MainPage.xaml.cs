120739393ed43dbceaa3904798389664bda7bc1a
Gerardo Gamas <kukulcan_gamas@hotmail.com>
2020-11-12 21:14:16 -0500

    Pantalla Principal

--- UILoginPage/UILoginPage/MainPage.xaml.cs
+++ UILoginPage/UILoginPage/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace UILoginPage
     public partial class MainPage : ContentPage
     {
         public MainPage()
+            //hola
         {
             InitializeComponent();
         }
